ddc09c92a8f3d0616ebb2b342ab7ffec8c65ad51
Tina <tinaachenn14@gmail.com>
2023-10-26 21:05:08 -0400

    Create petition sign up

--- scripts/main.js
+++ scripts/main.js
@@ -8,19 +8,28 @@ const toggleDarkMode = () => {
 }
 themeButton.addEventListener("click", toggleDarkMode);
 
+//adding signatures function
+const signNowButton = document.getElementById("sign-now-button");
+const signaturesDiv = document.querySelector('.signatures');
 
-// const title = document.querySelector('h1');
+const addSignature = (event) => {
+  event.preventDefault();
+  let name = document.getElementById("name");
+  let hometown = document.getElementById("hometown");
 
-// // Change an element's style
-// title.style.color = 'red';
-// title.style.font = 'Avenir';
-// title.style.fontSize = '25px';
+  const newSignature = document.createElement('p');
+  newSignature.innerText = `🖊️ ${name.value} from ${hometown.value} supports this.`
+  signaturesDiv.appendChild(newSignature);
 
+  let counter = document.getElementById("counter");
+  counter.remove();
+  count = count + 1;
+  const newCounter = document.createElement('p');
+  newCounter.setAttribute("id", "counter");
+  newCounter.innerText = `️ 🖊️ ${count} people have signed this petition and support this cause.`;
+  signaturesDiv.appendChild(newCounter);
+}
 
-// // Modify text
-// title.innerText = 'Chipped Cup';
+signNowButton.addEventListener('click', addSignature);
 
-
-// // Modify attributes
-// title.setAttribute('class', 'top-heading');
-// title.removeAttribute('id');
+let count = 3;
